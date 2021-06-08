5d81ebdf898dfd43a8731e8e27519cc4d266735f
Jian He <hexiaok@gmail.com>
2021-06-08 10:18:24 +0200

    React hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React, {Component} from "react"
+import React, {Component,useState} from "react"
 import randomcolor from "randomcolor"
 import Conditional from "./Conditional"
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      firstName: "",
-      lastName:""
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(event) {
-    const {name, value} = event.target
-    this.setState({
-      [name]: value
-    })
-  }
 
-  render() {
-    return (
-      <form>
-        <input type="text" value={this.state.firstName} name="firstName" placeholder="First Name" onChange={this.handleChange}/>
-        <input type="text" value={this.state.lastName} name="lastName" placeholder="Last Name" onChange={this.handleChange}/>
-      </form>
+function App() {
+  const [count, setCount] = useState(0)
+  function increment() {
+    setCount(
+      prevCount => prevCount +1
     )
   }
-
+  return (
+    <div> 
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
 
 }
+
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       firstName: "",
+//       lastName:""
+//     }
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+//   handleChange(event) {
+//     const {name, value} = event.target
+//     this.setState({
+//       [name]: value
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <input type="text" value={this.state.firstName} name="firstName" placeholder="First Name" onChange={this.handleChange}/>
+//         <input type="text" value={this.state.lastName} name="lastName" placeholder="Last Name" onChange={this.handleChange}/>
+//       </form>
+//     )
+//   }
+
+
+// }
 
 
 
